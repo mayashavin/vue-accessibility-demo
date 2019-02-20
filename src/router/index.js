f371131010a4b3b3ffc9bee0c53ff3218b5f3a13
Maya Shavin <maya@cloudinary.com>
2019-02-21 01:14:53 +0200

    jhlkj

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,43 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from '@/components/Home/Home';
-import TextBackgroundDemo from '@/components/TextBackgroundDemo/TextBackgroundDemo';
-import ImageManipulationDemo from '@/components/ImageManipulationDemo/ImageManipulationDemo';
-import ElementOrderDemo from '@/components/ElementOrderDemo/ElementOrderDemo';
-import Demo from '@/components/MainDemo/MainDemo';
+// import Vue from 'vue';
+// import Router from 'vue-router';
+// import Home from '@/components/Home/Home';
+// import TextBackgroundDemo from '@/components/TextBackgroundDemo/TextBackgroundDemo';
+// import ImageManipulationDemo from '@/components/ImageManipulationDemo/ImageManipulationDemo';
+// import ElementOrderDemo from '@/components/ElementOrderDemo/ElementOrderDemo';
+// import Demo from '@/components/MainDemo/MainDemo';
 
-Vue.use(Router);
+// Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/demo',
-      name: 'Demo',
-      component: Demo,
-    },
-    {
-      path: '/demo1',
-      name: 'ElementOrderDemo',
-      component: ElementOrderDemo,
-    },
-    {
-      path: '/demo2',
-      name: 'TextBackgroundDemo',
-      component: TextBackgroundDemo,
-    },
-    {
-      path: '/demo3',
-      name: 'ImageManipulation',
-      component: ImageManipulationDemo,
-    },
-    {
-      path: '*',
-      component: Home,
-    },
-  ],
-});
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home,
+//     },
+//     {
+//       path: '/demo',
+//       name: 'Demo',
+//       component: Demo,
+//     },
+//     {
+//       path: '/demo1',
+//       name: 'ElementOrderDemo',
+//       component: ElementOrderDemo,
+//     },
+//     {
+//       path: '/demo2',
+//       name: 'TextBackgroundDemo',
+//       component: TextBackgroundDemo,
+//     },
+//     {
+//       path: '/demo3',
+//       name: 'ImageManipulation',
+//       component: ImageManipulationDemo,
+//     },
+//     {
+//       path: '*',
+//       component: Home,
+//     },
+//   ],
+// });
