d1ebe9af49665f825f72d7957b178df09467b460
Maya Shavin <maya@cloudinary.com>
2019-03-29 08:32:30 +0300

    update project

---
+++ src/src/demo/Home/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <div class="option d--flex">
+      <h3>👑 Demo: Accessibility</h3>
+      <router-link tag="button" class="btn" to="demo">Show me</router-link>
+    </div>
+    <div class="option d--flex">
+      <h3>🤓 Demo: Element Order & Usage</h3>
+      <router-link tag="button" class="btn" to="demo1">Show me</router-link>
+    </div>
+    <div class="option d--flex">
+      <h3>👩🏻‍💻 Demo: Text ➕ Background (CSS Filter)</h3>
+      <router-link tag="button" class="btn" to="demo2">Show me</router-link>
+    </div>
+    <div class="option d--flex">
+      <h3>🎯 Demo: Image Manipulation (Color blind)</h3>
+      <router-link tag="button" class="btn" to="demo3">Show me</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {};
+  },
+  methods: {
+    goTo(path) {
+      this.$router.push(path);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 550px;
+    margin: auto;
+    padding: 1rem 0;
+    border: 1px solid #0a3464;
+    border-radius: 5px;
+  }
+
+  .option {
+    justify-content: space-between;
+    width: inherit;
+    padding: 0 1rem;
+    align-items: center;
+  }
+
+  .btn {
+    display: block;
+    margin: 1rem;
+    padding: 0.8rem 1rem;
+    font-size: 16px;
+    box-shadow: 2px 2px #b5b4b445;
+    border-radius: 5px;
+    cursor: pointer;
+    box-sizing: border-box;
+    background: #00800aa1;
+    color: white;
+  }
+</style>
+
+
---
+++ src/src/demo/ImageManipulationDemo/ImageManipulationDemo.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="demo-container">
+    <h1>Demo: Image Manipulation 🎯</h1>
+    <h2>Color blind support with Cloudinary</h2>
+    <div class="imageListWrapper" ref="listWrapper">
+      <div class="imageWrapper">
+        <CLDImage v-bind="normal" class="ph1" width="defaultWidth"/>
+        <h3 v-text="normal.alt"></h3>
+      </div>
+      <div class="imageWrapper">
+        <CLDImage v-bind="colorblind" class="ph1"/>
+        <h3 v-text="colorblind.alt"></h3>
+      </div>
+      <div class="imageWrapper">
+        <CLDImage v-bind="highcontrastColorblind" class="ph1"/>
+        <h3 v-text="highcontrastColorblind.alt"></h3>
+      </div>
+      <div class="imageWrapper">
+        <CLDImage v-bind="colorblindXray" class="ph1"/>
+        <h3 v-text="colorblindXray.alt"></h3>
+      </div>
+    </div>
+  </div>    
+</template>
+<script>
+export default {
+  name: 'ImageManipulationDemo',
+  data() {
+    return {
+      defaultWidth: 300,
+      normal: {
+        publicId: 'VueJS_Amsterdam_2018/p1c09qnt0212k34mp1g4pkrgh0j6',
+        alt: 'Normal green and red apples',
+      },
+      colorblind: {
+        publicId: 'VueJS_Amsterdam_2018/p1c09qnt0212k34mp1g4pkrgh0j6',
+        alt: 'Color blind support: stripes',
+        effect: 'assist_colorblind',
+      },
+      highcontrastColorblind: {
+        publicId: 'VueJS_Amsterdam_2018/p1c09qnt0212k34mp1g4pkrgh0j6',
+        alt: 'Color blind support: stripes in high contrast',
+        transformation: [{
+          effect: 'contrast:80',
+        }, {
+          effect: 'assist_colorblind',
+        }],
+      },
+      colorblindXray: {
+        publicId: 'VueJS_Amsterdam_2018/p1c09qnt0212k34mp1g4pkrgh0j6',
+        alt: 'Color blind support: Xray',
+        effect: 'assist_colorblind:x_ray',
+      },
+    };
+  },
+  mounted() {
+    const listWrapper = this.$refs.listWrapper;
+    listWrapper.style.setProperty('--columns', 4);
+  },
+};
+</script>
+<style lang="scss" scoped>
+  .imageListWrapper {
+    display: grid;
+    grid-template-columns: repeat(var(--columns), auto);
+    grid-gap: 1rem;
+    justify-content: center;
+  }
+</style>
+
